7ca2fd655ebbfe6334880be503a6127c5e7db776
Bunlai Kek <kekbunlai@gmail.com>
2020-12-03 10:13:47 +0700

    finish video 198

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
 
-class ProdutsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProdutsOverviewScreen extends StatefulWidget {
+  @override
+  _ProdutsOverviewScreenState createState() => _ProdutsOverviewScreenState();
+}
+
+class _ProdutsOverviewScreenState extends State<ProdutsOverviewScreen> {
+  var _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favourites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              )
+            ],
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                _showOnlyFavorites = true;
+              } else {
+                _showOnlyFavorites = false;
+              }
+              });
+              
+            },
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
-
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -4,10 +4,13 @@ import '../providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<Products>(context);
-    final products = productData.items;
+    final productsData = Provider.of<Products>(context);
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
